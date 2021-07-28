4c21f6341eb98d596d758b558795434b99e86c0a
Sebastian R. Papanikolaou Costa <11996369+super-sebajin@users.noreply.github.com>
2021-07-28 13:42:04 -0400

    Java implementations have been created

---
+++ GraphTheoryResearch/Java/GraphEdge.java
@@ -0,0 +1,23 @@
+//Author: Sebastian R. Papanikolaou Costa
+
+package GeneralCode.GraphTheoryResearch.Java;
+
+
+public class GraphEdge {
+
+
+    //Edge will have the exact same behaivor as a doubly linked list node, with out the data
+    class Edge {
+
+        GraphVertex.Vertex vertexA;
+        GraphVertex.Vertex vertexB;
+
+
+    } 
+
+    public static void main(String[] args){
+
+
+    }
+
+}
---
+++ GraphTheoryResearch/Java/GraphVertex.java
@@ -0,0 +1,32 @@
+//Author: Sebastian R. Papanikolaou Costa
+//Java implentation of the class "Graph Vertex" in UnidrectedGraph.py
+//Due to Java being strongly typed, this implementation will work with string data.
+
+package GeneralCode.GraphTheoryResearch.Java;
+import java.util.ArrayList;
+
+
+public class GraphVertex{
+
+
+    //Vertex will hold the data, degree and an incidence list in the form of an array
+    class Vertex{
+        String data;
+        int degree;
+        ArrayList<GraphEdge.Edge> incidenceList;
+
+        public Vertex(String value){
+            this.data = value;
+        }
+        
+
+        
+
+    }
+    public static void main(String[] args){
+        
+    }
+
+
+
+}
